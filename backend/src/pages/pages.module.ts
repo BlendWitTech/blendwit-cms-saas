ff301042bcec9004c1b94a4e1cf59dbde0286f01
BlendWitTech <blendwittech@gmail.com>
2026-02-03 22:15:15 +0545

    Fix: Export PagesService from PagesModule to resolve ThemesService dependency

--- backend/src/pages/pages.module.ts
+++ backend/src/pages/pages.module.ts
@@ -9,5 +9,6 @@ import { UsersModule } from '../users/users.module';
     imports: [PrismaModule, SeoMetaModule, UsersModule],
     controllers: [PagesController],
     providers: [PagesService],
+    exports: [PagesService],
 })
 export class PagesModule { }
