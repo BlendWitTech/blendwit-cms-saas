12e9bc8e4ea79171f5ee6a4cf5171f772fc19776
BlendWitTech <blendwittech@gmail.com>
2026-02-03 22:09:53 +0545

    Cleanup: Final removal of temporary debug scripts and logs

--- frontend/check-tiptap-2.js
+++
@@ -1,4 +0,0 @@
-const TiptapReact = require('@tiptap/react');
-console.log('Has BubbleMenu:', 'BubbleMenu' in TiptapReact);
-console.log('Has FloatingMenu:', 'FloatingMenu' in TiptapReact);
-console.log('Keys starting with B:', Object.keys(TiptapReact).filter(k => k.startsWith('B')));
